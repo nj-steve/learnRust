ddc04b39eba0d551a57db26905c0cf3975d92d86
feng <123@163.com>
2020-06-28 15:16:57 +0800

    add trait

---
+++ array/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let parents = [0,1,2,3,4,5,6,7,8,9,0];
+    let parent = &parents[..8];
+    for n in 0..parent.len(){
+        println!("nnnn = {}",parent[n]);
+    }
+
+    let mut multiparents =  [1;36];
+    for n in 0..6 {
+        multiparents[n*6 + 0] = 0 ;
+        multiparents[n*6 + 1] = 1 ;
+        multiparents[n*6 + 2] = 2 ;
+        multiparents[n*6 + 3] = 3 ;
+        multiparents[n*6 + 4] = 4 ;
+        multiparents[n*6 + 5] = 5 ;
+    }
+    for n in 0..multiparents.len(){
+        println!("multiparents = {}",multiparents[n]);
+    }
+
+    let mut multiparents2 =  [1;36];
+    for n in 0..3 {
+        multiparents2[n*14 + 0] = 0 ;
+        multiparents2[n*14 + 1] = 1 ;
+        multiparents2[n*14 + 2] = 2 ;
+        multiparents2[n*14 + 3] = 3 ;
+        multiparents2[n*14 + 4] = 4 ;
+        multiparents2[n*14 + 5] = 5 ;
+        multiparents2[n*14 + 6] = 6 ;
+        multiparents2[n*14 + 7] = 7 ;
+        if n < 2 {
+            multiparents2[n*14 + 8] = 8 ;
+            multiparents2[n*14 + 9] = 9 ;
+            multiparents2[n*14 + 10] = 10 ;
+            multiparents2[n*14 + 11] = 11 ;
+            multiparents2[n*14 + 12] = 12 ;
+            multiparents2[n*14 + 13] = 13 ;
+        }
+    }
+    for n in 0..multiparents2.len(){
+        println!("multiparents2 = {}",multiparents2[n]);
+    }
+    println!("Hello, world!");
+}
