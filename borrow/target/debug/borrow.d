ec63c20d517ff99bb7212c4977f7ca6d23d05e23
shxfbu <411791954@qq.com>
2020-07-20 14:52:46 +0800

    add

---
+++ borrow/target/debug/borrow.d
@@ -0,0 +1,1 @@
+/Users/mac/rustlearn/learnRust/borrow/target/debug/borrow: /Users/mac/rustlearn/learnRust/borrow/src/main.rs
---
+++ borrow/target/debug/deps/borrow-4db814f4c686e334.d
@@ -0,0 +1,5 @@
+/Users/mac/rustlearn/learnRust/borrow/target/debug/deps/borrow-4db814f4c686e334: src/main.rs
+
+/Users/mac/rustlearn/learnRust/borrow/target/debug/deps/borrow-4db814f4c686e334.d: src/main.rs
+
+src/main.rs:
